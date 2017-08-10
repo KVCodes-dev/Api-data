13c1c0aa4cf7e491fa0390f6312279c549900578
Akvaghela <30888033+Akvaghela@users.noreply.github.com>
2017-08-10 17:28:55 +0530

    Add files via upload

---
+++ AndroidVersion.java
@@ -0,0 +1,24 @@
+package com.example.prince.api_data;
+
+/**
+ * Created by prince on 02-Jun-17.
+ */
+
+public class AndroidVersion {
+
+    private String ver;
+    private String name;
+    private String api;
+
+    public String getVer() {
+        return ver;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getApi() {
+        return api;
+    }
+}
---
+++ Response.java
@@ -0,0 +1,17 @@
+package com.example.prince.api_data;
+
+/**
+ * Created by prince on 02-Jun-17.
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Response {
+
+    private List<AndroidVersion> android = new ArrayList<AndroidVersion>();
+
+    public List<AndroidVersion> getAndroid() {
+        return android;
+    }
+}
